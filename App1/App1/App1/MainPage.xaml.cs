a04554ef379fe6c0652401dc507c44b35dcc3877
AldoStevens <aldostevens2@gmail.com>
2020-11-13 14:49:46 +0200

    first commit

---
+++ App1/App1/App1/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+using Banking; 
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+
+        private BankAccount _account;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Bank fnb = new Bank("First National Bank", 4324, "Kenilworth");
+            Customer myNewCustomer = new Customer("7766445424", "10 me at home", "Bob", "The Builder");
+            fnb.AddCustomer(myNewCustomer);
+
+            _account = myNewCustomer.ApplyForBankAccount();
+        }
+
+        private void DepositButton_Clicked(object sender, EventArgs e)
+        {
+            decimal depositeAmount = 0;
+            var valid = decimal.TryParse(DepositAmountEntry.Text, out depositeAmount);
+            var reason = DepositReasonEntry.Text;
+
+            if (valid)
+            {
+                _account.DepositMoney(depositeAmount, DateTime.Now, reason);
+
+            }
+            else
+            {
+                DisplayAlert("Validation Error,", "Please Enter a Number", "Cancel");
+            }
+        }
+    }
+}
